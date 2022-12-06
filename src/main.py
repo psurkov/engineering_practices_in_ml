f518c2e7b66f757bb755a48600cacf53879f46b4
peter <petyo333@gmail.com>
2022-12-06 20:47:28 +0100

    HW4

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+import numpy
 from sklearn.datasets import make_moons
 
 from DecisionTreeClassifier import DecisionTreeClassifier
@@ -5,14 +6,16 @@ from visualization import draw_tree, plot_2d, plot_roc_curve
 
 
 def main():
-    noise = 0.35
-    X, y = make_moons(1500, noise=noise)
-    X_test, y_test = make_moons(200, noise=noise)
+    X = numpy.genfromtxt("../data/trainX.csv", delimiter=",")
+    y = numpy.genfromtxt("../data/trainY.csv",  delimiter=",")
+    X_test = numpy.genfromtxt("../data/testX.csv", delimiter=",")
+    y_test = numpy.genfromtxt("../data/testY.csv",  delimiter=",")
     tree = DecisionTreeClassifier(max_depth=5, min_samples_leaf=30)
     tree.fit(X, y)
-    plot_2d(tree, X, y)
-    plot_roc_curve(y_test, tree.predict_proba(X_test))
-    draw_tree(tree)
+    artifacts_path = "../artifacts/"
+    plot_2d(tree, X, y, artifacts_path + "plot.png")
+    plot_roc_curve(y_test, tree.predict_proba(X_test), artifacts_path + "roc_curve.png")
+    draw_tree(tree, artifacts_path + "tree.png")
 
 
 if __name__ == "__main__":
