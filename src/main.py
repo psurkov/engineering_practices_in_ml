053261e07725a365051a85c32e0f61d72f4e9fdb
peter <petyo333@gmail.com>
2022-12-18 16:22:45 +0100

    Create params.yaml for parameters, create prepare.py to generate data, use fixed seed in numpy

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
+import dvc.api
 import numpy
-from sklearn.datasets import make_moons
 
 from DecisionTreeClassifier import DecisionTreeClassifier
 from visualization import draw_tree, plot_2d, plot_roc_curve
@@ -10,8 +10,16 @@ def main():
     y = numpy.genfromtxt("../data/trainY.csv",  delimiter=",")
     X_test = numpy.genfromtxt("../data/testX.csv", delimiter=",")
     y_test = numpy.genfromtxt("../data/testY.csv",  delimiter=",")
-    tree = DecisionTreeClassifier(max_depth=5, min_samples_leaf=30)
+
+    params = dvc.api.params_show()
+    max_depth = params["train"]["max_depth"]
+    min_samples_leaf = params["train"]["min_samples_leaf"]
+    tree = DecisionTreeClassifier(max_depth=max_depth, min_samples_leaf=min_samples_leaf)
+
+    numpy.random.seed(params["train"]["seed"])
+
     tree.fit(X, y)
+
     artifacts_path = "../artifacts/"
     plot_2d(tree, X, y, artifacts_path + "plot.png")
     plot_roc_curve(y_test, tree.predict_proba(X_test), artifacts_path + "roc_curve.png")
---
+++ src/prepare.py
@@ -0,0 +1,24 @@
+import numpy
+from sklearn.datasets import make_moons
+import dvc.api
+
+
+def main():
+    params = dvc.api.params_show()
+    numpy.random.seed(params["prepare"]["seed"])
+
+    noise = params["prepare"]["data"]["noise"]
+
+    X, y = make_moons(params["prepare"]["data"]["train"]["n_samples"], noise=noise)
+    X_test, y_test = make_moons(
+        params["prepare"]["data"]["test"]["n_samples"], noise=noise
+    )
+
+    numpy.savetxt("../data/trainX.csv", X, delimiter=",")
+    numpy.savetxt("../data/trainY.csv", y, delimiter=",")
+    numpy.savetxt("../data/testX.csv", X_test, delimiter=",")
+    numpy.savetxt("../data/testY.csv", y_test, delimiter=",")
+
+
+if __name__ == "__main__":
+    main()
